5c37df497d684a21c8c8b2523aac56791a6815a3
veroglez <veronica.glezperez@gmail.com>
2019-10-10 01:55:16 +0200

    Cardlist component and test

--- client/src/Components/CardList/CardList.js
+++ client/src/Components/CardList/CardList.js
@@ -1,44 +1,28 @@
 import React, { PureComponent } from 'react';
-import { arrayOf, shape, number, string } from 'prop-types';
+import { arrayOf, shape } from 'prop-types';
 import { Card } from '..';
-import { ConsumerData } from '../../common/context/context';
 import style from './CardList.module.scss';
 
 class CardList extends PureComponent {
   static propTypes = {
-    dishes: arrayOf(shape({})),
+    dishes: arrayOf(shape({})).isRequired,
   };
-
-  static defaultProps = {
-    dishes: undefined,
-  };
-
-  state = { modal:false }
-
-  onModal = () => {
-    this.setState({ modal: true })
-
-  }
 
   render() {
-    const { onModal, props: { dishes }, state: { modal } } = this;
+    const { props: { dishes } } = this;
 
     return (
-      <ConsumerData>
-        {context => (
-          <div className={style.cardList}>
-            <div role="presentation" onClick={() => onModal()}>Cart</div>
-            {dishes.map((dish, index) => (
-              <Card key={index.toString()} image={dish.image} title={dish.name} price={dish.price} />
-            ))}
-            {modal && (
-              <div className={style.modal}>
-                Total: {context.total}
-              </div>
-            )}
-          </div>
-        )}
-      </ConsumerData>
+      <div className={style.cardList}>
+        {dishes.map((dish, index) => (
+          <Card
+            key={index.toString()}
+            index={index}
+            image={dish.image}
+            title={dish.name}
+            price={dish.price}
+          />
+        ))}
+      </div>
     );
   }
 }
---
+++ client/src/Components/CardList/CardList.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import CardList from './CardList';
+import { ProviderData } from '../../common/context/context';
+
+
+const DISHES = [
+  { name: 'lorem ipsum 1', image: '/image.png', price: 20 },
+  { name: 'lorem ipsum 2', image: '/image.png', price: 30 },
+];
+const DEFAULTS = {
+  l10n: require('../../common/l10n'),
+};
+
+const HOC = inherit => (
+  <ProviderData>
+    <CardList {...DEFAULTS} {...inherit} />
+  </ProviderData>
+);
+
+describe('<CardList>', () => {
+  it('when {dishes}', () => {
+    const tree = renderer.create(<HOC dishes={DISHES} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
