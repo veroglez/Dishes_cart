3932358b0b37f772edcd395a260a1129a734d235
veroglez <veronica.glezperez@gmail.com>
2019-10-10 02:39:33 +0200

    Modifying index

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import './index.css';
+import './index.module.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
